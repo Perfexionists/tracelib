b728ce5661784a004b84d7538499a69b0d497fa4
Vladimir Drobny <xdrobny2@fi.muni.cz>
2025-12-21 21:42:34 +0100

    Optimize getNextEvent for less copying

--- src/perfFolded.cpp
+++ src/perfFolded.cpp
@@ -42,54 +42,50 @@ void PerfFoldedParser::parseMetadata() {
 
 std::unique_ptr<Event> PerfFoldedParser::getNextEvent() {
     // Retrieve a line from the file
-    std::string currentLine;
-    if (!std::getline(this->traceFile, currentLine)) {
+    if (!std::getline(this->traceFile, this->currentLine)) {
         this->currentLine = "";
         return nullptr;
     }
-    this->currentLine = currentLine;
-    if (currentLine.empty() or currentLine.find_first_not_of(" \t\n\v\f\r") == std::string::npos) {
+    if (this->currentLine.empty() or this->currentLine.find_first_not_of(" \t\n\v\f\r") == std::string::npos) {
         return std::move(this->getNextEvent());
     }
     this->numberOfEvents++;
 
     // Parse the line
     size_t pos;
-    if ( (pos = currentLine.find(' ')) == std::string::npos ) {
+    if ( (pos = this->currentLine.find(' ')) == std::string::npos ) {
         std::cerr << "[E]: Unexpected format of trace file!" << std::endl;
         exit(1);
     }
-    std::string stackSampleString = currentLine.substr(0, pos); // TODO stringviews here
-    const std::string sampleCntString = currentLine.substr(pos+1);
+    std::string stackSampleString = this->currentLine.substr(0, pos); // TODO stringviews here
+    const std::string sampleCntString = this->currentLine.substr(pos+1);
     const long long int sampleCnt = std::stoll(sampleCntString);
 
     if ( (pos = stackSampleString.find(';')) == std::string::npos ) {
         std::cerr << "[E]: Unexpected format of trace file!" << std::endl;
         exit(1);
     }
-    const std::string processName = stackSampleString.substr(0, pos);
+    auto event = std::make_unique<PerfFoldedEvent>(Event::STACK_SAMPLE, "");
+    event->data = std::make_unique<PerfFoldedEventData>(sampleCnt);
+
+    event->processName = stackSampleString.substr(0, pos);
     stackSampleString = stackSampleString.substr(pos+1) + ';';
 
-    int processId = this->processIdCounter;
-    if (auto it = this->processNameToProcessIdMap.find(processName); it != this->processNameToProcessIdMap.end()) {
-        processId = it->second;
+    if (auto it = this->processNameToProcessIdMap.find(event->processName); it != this->processNameToProcessIdMap.end()) {
+        event->pid = it->second;
     } else {
-        this->processNameToProcessIdMap.emplace(processName, this->processIdCounter++);
+        event->pid = this->processIdCounter;
+        this->processNameToProcessIdMap.emplace(event->processName, this->processIdCounter++);
     }
 
-    std::vector<std::string> stackSample;
     size_t start = 0;
     size_t end = 0;
     while ( (end = stackSampleString.find(';', start)) != std::string::npos ) {
-        stackSample.push_back(stackSampleString.substr(start, end-start));
+        event->stackSample.push_back(stackSampleString.substr(start, end-start));
         start = end + 1;
     }
+    event->name = event->stackSample.back();
 
-    auto event = std::make_unique<PerfFoldedEvent>(Event::STACK_SAMPLE, stackSample.back());
-    event->stackSample = stackSample;
-    event->processName = processName;
-    event->pid = processId;
-    event->data = std::make_unique<PerfFoldedEventData>(sampleCnt);
     return event;
 }
 
